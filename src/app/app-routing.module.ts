a98d69620185b385e1affc9d7ea8eaa1afe287f8
josmig <miguepantigoso@gmail.com>
2024-05-26 21:38:04 -0500

    config + routingModuloe + routerOutlet basic config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/homePage/home-page.component';
+import { AboutPageComponent } from './shared/pages/aboutPage/about-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: HomePageComponent
+  },
+  {
+    path:'about',
+    component: AboutPageComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+//Components
+import { AppComponent } from './app.component';
+
+//Modulos
+import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
